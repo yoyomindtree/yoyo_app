f04d4ceb3e3a4e96f39298664be0442f5576aa1e
YoYo Mindtree <yoyomindtree@gmail.com>
2019-05-19 22:36:26 +0530

    Added code for sign out

--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { FirebaseService } from './../../services/firebase.servce';
 import { LoginService } from './../../services/login.service';
 import { Component, OnInit } from '@angular/core';
-
+import * as emailjs from 'emailjs-com';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -14,5 +14,18 @@ export class LoginComponent implements OnInit {
   onLoginWithGoogle() {
     this.loginService.LoginInWithGoogle().then((data) => console.log(data));
   }
-  CheckCon() {}
+  CheckCon() {
+    var templateParams = {
+      name: 'Testing123',
+      to_name: 'Raghavendra',
+    };
+    emailjs.send('yoyo', 'template_5bhTnqFg', templateParams, 'user_LqyB0x9nwHbehnc2Fp7G1').then(
+      function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+      },
+      function(err) {
+        console.log('FAILED...', err);
+      },
+    );
+  }
 }
--- src/app/shared/services/login.service.ts
+++ src/app/shared/services/login.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+
+// firebase imports
 import { auth } from 'firebase';
+import { AngularFireAuth } from '@angular/fire/auth';
+
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
   constructor(private authService: AngularFireAuth) {}
-  LoginInWithGoogle() {
+  /**
+   * method to login with google
+   */
+  public LoginInWithGoogle(): Promise<auth.UserCredential> {
     return this.authService.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  /**
+   * method to log out
+   */
+  public LogOut() {
+    this.authService.auth.signOut();
   }
 }
