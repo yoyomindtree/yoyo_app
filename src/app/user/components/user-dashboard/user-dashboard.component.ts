9bf1021c2154bd5f837a043dd7eac4c8468af79f
yoyomindtree@gmail.com <M1034167@mindtree.com>
2019-05-21 12:10:49 +0530

    Added card design

--- src/app/user/components/user-dashboard/user-dashboard.component.ts
+++ src/app/user/components/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,7 @@
+import { GiftModel } from './../../../shared/model/gift.model';
+import { FirebaseService } from 'src/app/shared/services/firebase.servce';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/internal/operators/map';
 
 @Component({
   selector: 'app-user-dashboard',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDashboardComponent implements OnInit {
 
-  constructor() { }
+  public giftsForSending: GiftModel[];
+  constructor(private firebaseService: FirebaseService) { }
 
   ngOnInit() {
+    this.getGifts();
+  }
+  getGifts() {
+    this.firebaseService
+    .getAllGifts()
+    .snapshotChanges()
+    .pipe(map((changes) => changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))))
+    .subscribe((data: GiftModel[]) => {
+this.giftsForSending = data;
+    });
   }
 
 }
