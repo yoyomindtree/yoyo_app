23693053ba775f4e36e4701ccc93733ea312730f
YoYo Mindtree <yoyomindtree@gmail.com>
2019-05-21 15:30:32 +0530

    added Admin feature

--- src/app/admin/components/admin-addgiftpopup/admin-addgiftpopup.component.ts
+++ src/app/admin/components/admin-addgiftpopup/admin-addgiftpopup.component.ts
@@ -1,20 +1,30 @@
 import { AdminFireService } from './../../services/admin-fire.service';
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { GiftModel } from 'src/app/shared/model/gift.model';
 @Component({
   selector: 'app-admin-addgiftpopup',
   templateUrl: './admin-addgiftpopup.component.html',
   styleUrls: ['./admin-addgiftpopup.component.css'],
 })
-export class AdminAddgiftpopupComponent implements OnInit {
+export class AdminAddgiftpopupComponent implements OnInit, OnDestroy {
+  // property subscription
+  subscription: Subscription;
+  // property points
+  public points: number;
+
+  /**
+   * Form builder
+   */
   giftForm = this.formBuilder.group({
-    giftName: ['', Validators.required],
-    discription: ['', Validators.required],
-    category: ['', Validators.required],
-    vendor: ['', Validators.required],
-    points: ['', Validators.required],
-    discount: ['', Validators.required, Validators.min(1)],
+    giftName: ['', [Validators.required]],
+    discription: ['', [Validators.required]],
+    category: ['', [Validators.required]],
+    vendor: ['', [Validators.required]],
+    points: ['', [Validators.required]],
+    discount: ['', [Validators.required, Validators.min(0), this.validateDiscount.bind(this)]],
     quantity: ['', [Validators.min(1), Validators.required]],
     imagePath: ['', [Validators.required]],
   });
@@ -25,8 +35,33 @@ export class AdminAddgiftpopupComponent implements OnInit {
     private adminFireSerive: AdminFireService,
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // subscription to get the property whenever the input point filed will change.
+    this.subscription = this.giftForm.get('points').valueChanges.subscribe((formValue) => {
+      this.points = formValue;
+    });
+  }
+  ngOnDestroy(): void {
+    // unsubscription.
+    this.subscription.unsubscribe();
+  }
+  /**
+   * Method will get called once admin submits form
+   */
   onSubmit() {
-    console.log(this.giftForm);
+    //const gift = new GiftModel();
+  }
+  /**
+   *
+   * @param control -FormControl
+   * Custome validator method
+   */
+  validateDiscount(control: FormControl): { [s: string]: boolean } {
+    if (this.points < control.value) {
+      return { isSmaller: true };
+    } else {
+      return null;
+    }
+    // return { isSmaller: true };
   }
 }
