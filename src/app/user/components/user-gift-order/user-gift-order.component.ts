32fc3d2bb1289c3e381be0f684c9c32b0dbcdc9d
yoyomindtree@gmail.com <M1034167@mindtree.com>
2019-05-24 13:19:55 +0530

    Gift fetched from resolver

--- src/app/user/components/user-gift-order/user-gift-order.component.ts
+++ src/app/user/components/user-gift-order/user-gift-order.component.ts
@@ -1,4 +1,8 @@
+import { GiftModel } from 'src/app/shared/model/gift.model';
+import { GiftResolverService } from './../../../shared/services/gift-resolver.service';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-gift-order',
@@ -6,8 +10,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-gift-order.component.css']
 })
 export class UserGiftOrderComponent implements OnInit {
+  public giftDetail: GiftModel;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.data.pipe(map((data: any) => data.gift[0])).subscribe((res) => {
+      this.giftDetail = res;
+    });
+  }
 
   ngOnInit() {
   }
