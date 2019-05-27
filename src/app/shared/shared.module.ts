f0c9f094985837092e4adb71c73d67ebd8e405ec
YoYo Mindtree <yoyomindtree@gmail.com>
2019-05-27 15:17:48 +0530

    added subscription and removed common table

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,13 +7,12 @@ import { AngularMaterialModule } from '../angular-material.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { TableComponent } from './components/table/table.component';
 import { EditUserDialogComponent } from './components/edit-user-dialog/edit-user-dialog.component';
 
 @NgModule({
-  declarations: [LoginComponent, FooterComponent, HeaderComponent, TableComponent, EditUserDialogComponent],
+  declarations: [LoginComponent, FooterComponent, HeaderComponent, EditUserDialogComponent],
   imports: [CommonModule, AngularMaterialModule, ReactiveFormsModule, FormsModule, HttpClientModule],
-  exports: [LoginComponent, HeaderComponent, FooterComponent, TableComponent],
-  entryComponents: [EditUserDialogComponent]
+  exports: [LoginComponent, HeaderComponent, FooterComponent],
+  entryComponents: [EditUserDialogComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
--- src/app/user/components/user-history/user-history.component.ts
+++ src/app/user/components/user-history/user-history.component.ts
@@ -1,15 +1,16 @@
 import { FirebaseService } from 'src/app/shared/services/firebase.service';
 import { HistoryModel } from './../../../shared/model/history.model';
 import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-history',
   templateUrl: './user-history.component.html',
   styleUrls: ['./user-history.component.css'],
 })
-export class UserHistoryComponent implements OnInit {
+export class UserHistoryComponent implements OnInit, OnDestroy {
   // gets or sets the data source.
   public dataSource = new MatTableDataSource();
   // colums config for the userlist table
@@ -19,7 +20,7 @@ export class UserHistoryComponent implements OnInit {
   // param to gets or sets the sort
   @ViewChild(MatSort) sort: MatSort;
   // subscription variable
-  private subscriptionl;
+  private subscription: Subscription;
   constructor(private firebaseService: FirebaseService) {}
 
   ngOnInit() {
@@ -29,7 +30,7 @@ export class UserHistoryComponent implements OnInit {
    * get the history of the user.
    */
   public getHistory(): void {
-    this.firebaseService
+    this.subscription = this.firebaseService
       .getHistory()
       .snapshotChanges()
       .pipe(map((changes) => changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))))
@@ -40,5 +41,10 @@ export class UserHistoryComponent implements OnInit {
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
       });
+  }
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
